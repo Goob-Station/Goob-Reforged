279c65741f52fbcf8e7771a2b3196d07d67fa1a5
Roudenn <romabond091@gmail.com>
2026-05-09 20:37:13 +0300

    Add entry points to .Common modules

---
+++ Modules/GoobStation/Content.Goobstation.Common/Entry/EntryPoint.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.ContentPack;
+
+namespace Content.Goobstation.Common.Entry;
+
+public sealed class EntryPoint : GameShared
+{
+    public override void PreInit()
+    {
+        IoCManager.InjectDependencies(this);
+    }
+}
