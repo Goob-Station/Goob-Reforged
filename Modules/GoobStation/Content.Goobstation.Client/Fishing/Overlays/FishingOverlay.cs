29fa04a6287839507b1001e8d72ffcef6122268c
Roudenn <romabond091@gmail.com>
2026-05-09 21:53:32 +0300

    I hate this

--- Modules/GoobStation/Content.Goobstation.Client/Fishing/Overlays/FishingOverlay.cs
+++ Modules/GoobStation/Content.Goobstation.Client/Fishing/Overlays/FishingOverlay.cs
@@ -40,7 +40,7 @@ public sealed class FishingOverlay : Overlay
         _progressColor = _entManager.System<ProgressColorSystem>();
 
         // Load the progress bar texture
-        var sprite = new SpriteSpecifier.Rsi(new("/Textures/_Goobstation/Interface/Misc/fish_bar.rsi"), "icon");
+        var sprite = new SpriteSpecifier.Rsi(new ResPath("/Textures/Interface/Misc/fish_bar.rsi"), "icon");
         _barTexture = _entManager.EntitySysManager.GetEntitySystem<SpriteSystem>().Frame0(sprite);
     }
 
