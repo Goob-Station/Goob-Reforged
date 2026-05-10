31776b5e07a7103976fc554f7d7002d99f93f0e1
Roudenn <romabond091@gmail.com>
2026-05-10 17:14:13 +0300

    Revert "Upstream merge (#49)"
    This reverts commit 829a6c0b288842c5f255ca5efc1357895ab44c48.

--- Content.Shared/Destructible/SharedDestructibleSystem.cs
+++ Content.Shared/Destructible/SharedDestructibleSystem.cs
@@ -2,10 +2,10 @@ using Content.Shared.Damage.Systems;
 
 namespace Content.Shared.Destructible;
 
-public abstract partial class SharedDestructibleSystem : EntitySystem
+public abstract class SharedDestructibleSystem : EntitySystem
 {
     // TODO: I don't really like this but this is out of scope to re-do destructible triggers while refactoring damageable
-    [Dependency] public DamageableSystem Damageable = default!;
+    [Dependency] public readonly DamageableSystem Damageable = default!;
 
     /// <summary>
     /// Force entity to be destroyed and deleted.
